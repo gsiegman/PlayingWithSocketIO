97b4d9d5911dea390a68e6416ace193bb9f4a937
Glenn Siegman <gsiegman@gsiegman.com>
2011-05-31 02:02:00 -0400

    update client js for colors

--- static/feed.js
+++ static/feed.js
@@ -1,13 +1,12 @@
 function msgReceived(msg){
     console.log(eval("(" + msg + ")"));
     var data = eval("(" + msg + ")");
-    $firstName.html(data.first_name);
-    $lastName.html(data.last_name);
+    $color_span.html(data.color);
+    $("body").css("background-color", data.color);
 }
 
 $(document).ready(function(){
-    $firstName = $("#first_name");
-    $lastName = $("#last_name");
+    $color_span = $("#color");
     
     var socket = new io.Socket(null, {port:3000});
     socket.connect();
